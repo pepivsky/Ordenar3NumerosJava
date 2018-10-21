f15f05f747ab73f673861709b19ec12804d8926e
Jose Manuel Paredes Ramirez <ironglas68@gmail.com>
2018-10-21 11:29:57 -0500

    Comentarios borrados

--- src/ordenar3num/Ordenar3Num.java
+++ src/ordenar3num/Ordenar3Num.java
@@ -75,43 +75,5 @@ public class Ordenar3Num {
                 
 
 
-
-/*if ((numero1 <= numero2) && (numero1 <= numero3)) {
-            menor = numero1;
-            
-        if (numero2 <= numero3){
-            medio = numero2;
-            mayor = numero3;
-        }else{
-            medio = numero3;
-            mayor = numero2;
-        }
-
-        } else if ((numero2 <= numero1 ) &&(numero2 < numero3)){
-            
-            menor = numero2;
-            
-            if (numero1 <= numero3){
-                medio = numero1;
-                mayor = numero3;
-        }else{
-                medio = numero3;
-                mayor = numero1;
-                }
-            }else{
-            menor = numero3;
-            
-            if(numero1 <= numero2){
-                medio = numero1;
-                mayor = numero2;
-                }else{
-                medio = numero2;
-                mayor = numero1;
-            }
-            }
-    System.out.println("menor= " + menor + "\nMedio = " + medio + "\nMayor= " + mayor);
-    System.out.println("------------\nmayor= " + mayor + "\nMedio = " + medio + "\nMenor= " + menor);*/
-
-
     
     
